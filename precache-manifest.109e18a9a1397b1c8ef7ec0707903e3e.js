919153e1956a5c807e6f2151a042f860670e62b4
SebastianLabadie <sebastianlabadie.contact@gmail.com>
2020-08-29 19:48:22 -0300

    Updates

---
+++ precache-manifest.109e18a9a1397b1c8ef7ec0707903e3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d6bdbadbc1ba316da0db052da338484",
+    "url": "/react-todo-list-app/index.html"
+  },
+  {
+    "revision": "15e141523ddce85a380b",
+    "url": "/react-todo-list-app/static/css/main.9effc2ee.chunk.css"
+  },
+  {
+    "revision": "84cd9b15470a3f003945",
+    "url": "/react-todo-list-app/static/js/2.674b2033.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-list-app/static/js/2.674b2033.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15e141523ddce85a380b",
+    "url": "/react-todo-list-app/static/js/main.53d48d51.chunk.js"
+  },
+  {
+    "revision": "a7564e60f717b331cc2b",
+    "url": "/react-todo-list-app/static/js/runtime-main.055f47f4.js"
+  }
+]);
